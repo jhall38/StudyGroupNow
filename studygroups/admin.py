6db46f2eef81c219d28755a9fbaeef1f8940e4b4
Josh Hall <jhall38@uw.edu>
2016-08-11 09:35:15 +0000

    adds initial models

--- studygroups/admin.py
+++ studygroups/admin.py
@@ -1,2 +1,5 @@
 from django.contrib import admin
+from .models import StudyGroup, UserInfo
 
+admin.site.register(StudyGroup)
+admin.site.register(UserInfo)
--- studygroups/models.py
+++ studygroups/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class StudyGroup(models.Model):
+	name = models.CharField(max_length=100)
+	manager = models.ForeignKey('auth.User')
+	lon = models.FloatField()
+	lat = models.FloatField()
+	location_desc = models.CharField(max_length=500)
+	stat_time = models.DateTimeField(auto_now_add=True)
+	end_time = models.DateTimeField(blank=True)
 
+class UserInfo(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	phone_number = models.CharField(max_length=20, blank=True)
+	facebook_link = models.CharField(max_length=200, blank=True)
+	nickname = models.CharField(max_length=50, blank=True)		
