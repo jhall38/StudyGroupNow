51df261f9f834703c07dd7f9e39512fb3591110c
Josh Hall <jhall38@uw.edu>
2016-08-14 12:02:27 +0000

    updates scheme to include locations

--- studygroups/admin.py
+++ studygroups/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import StudyGroup, UserInfo
+from .models import StudyGroup, UserInfo, Location
 
 admin.site.register(StudyGroup)
 admin.site.register(UserInfo)
+admin.site.register(Location)
--- studygroups/models.py
+++ studygroups/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Location(models.Model):
+	name = models.CharField(max_length=50)
+	address = models.CharField(max_length=500)
+
 class StudyGroup(models.Model):
 	name = models.CharField(max_length=100)
 	manager = models.ForeignKey('auth.User')
-	lon = models.FloatField()
-	lat = models.FloatField()
+	location = models.ForeignKey(Location)
 	course_code = models.CharField(max_length=15)
 	location_desc = models.CharField(max_length=500)
 	stat_time = models.DateTimeField(auto_now_add=True)
@@ -15,4 +18,6 @@ class UserInfo(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	phone_number = models.CharField(max_length=20, blank=True)
 	facebook_link = models.CharField(max_length=200, blank=True)
-	nickname = models.CharField(max_length=50, blank=True)		
+	nickname = models.CharField(max_length=50, blank=True)	
+
+
