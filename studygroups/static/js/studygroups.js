ac8ee09c3cb1897ea00275b6216b80a16a04451f
jeff-ma <jefm206@gmail.com>
2016-08-14 16:56:29 -0700

    added map marker code

--- studygroups/static/js/studygroups.js
+++ studygroups/static/js/studygroups.js
@@ -1,25 +1,29 @@
 /* google map */
-var myCenter=new google.maps.LatLng(51.508742,-0.120850);
+var myCenter = new google.maps.LatLng(47.655078,-122.307882);
 
-function initialize()
-{
-var mapProp = {
-  center:myCenter,
-  zoom:5,
-  mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
+function initialize() {
+	var mapProp = {
+	  center:myCenter,
+	  zoom:15,
+	  mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
 
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	var markers = [['111', 'seattle', 98103, 47.65895, -122.307636 ],['321', 'seattle', 98103, 47.656639, -122.310318]]; /* {{django list of addresses and coordinates}} */
 
-var marker=new google.maps.Marker({
-  position:myCenter,
-  });
-
-marker.setMap(map);
+	for(i = 0; i < markers.length; i++) {
+		var coordinates = new google.maps.LatLng(markers[i][3], markers[i][4]);
+		var address = markers[i][0] + ' ' + markers[i][1] + ' ' + markers[i][2];
+		var marker = new google.maps.Marker({
+			position: coordinates,
+			map: map,
+			title: address,
+			icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + i + '|FF0000|000000'
+		});
+	}
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-
 /* end map */
 
 /* search page */
