5948a40c46ae371721da20543f216590ef98e92f
jeff-ma <jefm206@gmail.com>
2016-08-15 07:21:37 -0700

    Changed javascript for map to display search results markers

--- studygroups/static/js/studygroups.js
+++ studygroups/static/js/studygroups.js
@@ -1,6 +1,6 @@
 /* google map */
 /* api key below */
-/* AIzaSyBYxOP1Eb_CAGqLl2XrZBpADTaeV40MuBA */
+/* AIzaSyBYxOP1Eb_CAGqLl2XrZBpADTaeV40MuBA 
 var myCenter = new google.maps.LatLng(47.655078,-122.307882);
 
 function initialize() {
@@ -11,7 +11,7 @@ function initialize() {
 	};
 
 	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-	var markers = [['111', 'seattle', 98103, 47.65895, -122.307636 ],['321', 'seattle', 98103, 47.656639, -122.310318]]; /* {{django list of addresses and coordinates}} */
+	var markers = [['111', 'seattle', 98103, 47.65895, -122.307636 ],['321', 'seattle', 98103, 47.656639, -122.310318]]; /* {{django list of addresses and coordinates}} 
 
 	for(i = 0; i < markers.length; i++) {
 		var coordinates = new google.maps.LatLng(markers[i][3], markers[i][4]);
@@ -26,7 +26,7 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-/* end map */
+ end map */
 
 /* search page 
 toggle the search result info*/
