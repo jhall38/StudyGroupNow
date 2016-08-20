dd262c070ca8132249edc2e1b747667de5acd4ea
Josh Hall <jhall38@uw.edu>
2016-08-20 20:05:35 +0000

    serves static files from s3

---
+++ static/js/ajax.js
@@ -0,0 +1,48 @@
+$(function(){
+	$('#search_courses').click(function(){
+		$.ajax({
+			type: "POST",
+			url: "search/",
+			data: {
+				'search_text' : $('#search').val(),
+				'csrfmiddlewaretoken' : $("input[name=csrfmiddlewaretoken]").val()
+			},
+			success: searchSuccess,
+			dataType: 'html'
+		});
+	});
+	$('#newlocation').click(function(){
+		$.ajax({
+			type: "POST",
+			url: "new_location/",
+			data: {
+				'new_location_name' : $('#location_name').val(),
+				'new_location_address' : $('#location_address').val(),
+				'csrfmiddlewaretoken' : $("input[name=csrfmiddlewaretoken]").val()
+			},
+			success: newLocationSuccess,
+			dataType: 'html'
+		});
+	});
+	
+	$.get("/load_courses/", function(data) {
+		$('#classes').html(data);
+	});
+	$.get("/load_locations/", function(data) {
+		$('#locations').html(data)
+		$("#locations").val($('#selected').val());		
+	});
+});
+
+function searchSuccess(data, textStatus, jqXHR){
+	$('#results').html(data);
+
+}
+
+function newLocationSuccess(data, textStatus, jqXHR){
+	$('#locations').html(data);
+	$('#locations').val($('#location_name').val());
+}
+
+
+
