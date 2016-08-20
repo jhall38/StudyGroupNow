7491114dc31e989ffb00b2ee2a209e797bb57653
jeff-ma <jefm206@gmail.com>
2016-08-20 16:52:48 -0700

    added validation to login page

--- static/js/studygroups.js
+++ static/js/studygroups.js
@@ -1,38 +1,13 @@
-/* google map */
-/* api key below */
-/* AIzaSyBYxOP1Eb_CAGqLl2XrZBpADTaeV40MuBA 
-var myCenter = new google.maps.LatLng(47.655078,-122.307882);
 
-function initialize() {
-	var mapProp = {
-	  center:myCenter,
-	  zoom:15,
-	  mapTypeId:google.maps.MapTypeId.ROADMAP
-	};
-
-	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-	var markers = [['111', 'seattle', 98103, 47.65895, -122.307636 ],['321', 'seattle', 98103, 47.656639, -122.310318]]; /* {{django list of addresses and coordinates}} 
-
-	for(i = 0; i < markers.length; i++) {
-		var coordinates = new google.maps.LatLng(markers[i][3], markers[i][4]);
-		var address = markers[i][0] + ' ' + markers[i][1] + ' ' + markers[i][2];
-		var marker = new google.maps.Marker({
-			position: coordinates,
-			map: map,
-			title: address,
-			icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + i + '|FF0000|000000'
-		});
-	}
+/* validate login page */
+function validateLogin() {
+    var username = document.forms["login"]["username"].value;
+    var password = document.forms["login"]["password"].value;
+    if (username == null || username == "") {
+        alert("Please enter a username");
+        return false;
+    } else if (password == null || password == "") {
+    	alert("Please enter a password");
+        return false;
+    }
 }
-
-google.maps.event.addDomListener(window, 'load', initialize);
- end map */
-
-/* search page 
-toggle the search result info*/
-/*$(document).ready(function(){
-    $(".toggle-info").click(function(){
-        $(this).children(".search-info").slideToggle();
-    });
-/*});*/
-
