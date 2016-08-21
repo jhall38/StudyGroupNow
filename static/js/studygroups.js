ffaad6a7e45566c9c61fbb4379ca96d3f87da303
jeff-ma <jefm206@gmail.com>
2016-08-20 18:11:31 -0700

    added form validation for sign up page

--- static/js/studygroups.js
+++ static/js/studygroups.js
@@ -11,3 +11,28 @@ function validateLogin() {
         return false;
     }
 }
+
+/* validate signup page */
+function validateSignup() {
+    var username = document.forms["signup"]["username"];
+    var email = document.forms["signup"]["email"];
+    var regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    var password = document.forms["signup"]["password"];
+    var confirmpassword = document.forms["signup"]["confirmpassword"];
+    if (username.value == null || username.value == "") {
+        document.getElementById("errors").innerHTML = "Please enter a username!";
+        return false;
+    } else if (email.value.search(regex) == -1) {
+        document.getElementById("errors").innerHTML = "Please enter a valid email address!";
+        return false;
+    } else if (password.value == null || password.value == "") {
+         document.getElementById("errors").innerHTML = "Please enter a password!";
+        return false;
+    } else if (confirmpassword.value == null || confirmpassword.value == "") {
+        document.getElementById("errors").innerHTML = "Please confirm your password!";
+        return false;
+    } else if (password.value != confirmpassword.value) {
+         document.getElementById("errors").innerHTML = "Passwords do not match try again!";
+        return false;
+    }
+}
